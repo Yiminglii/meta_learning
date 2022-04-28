baf48b5c0c43fcb75b438426c02aeba379c98d7c
Yiminglii <44026243+Yiminglii@users.noreply.github.com>
2022-04-28 16:41:57 +0800

    Update meta_train.py

--- meta_train.py
+++ meta_train.py
@@ -14,6 +14,7 @@ class meta:
                 patch_size=8,
                 pooling_size=2,
                 in_channel_num=3,
+                target=-1
 
     ):
     
@@ -22,6 +23,7 @@ class meta:
         self.model1=model1
         self.model2=model2
         self.model3=model3
+        self.target=target
 
         self.meta_g=meta_generater(patch_size,pooling_size,in_channel_num).to(device)
         self.optimizer=torch.optim.Adam(self.meta_g.parameters(),lr=0.001)
@@ -34,15 +36,18 @@ class meta:
                 images,labels=data
                 images=images.to(self.device)
                 labels=labels.to(self.device)
-                pgd=self.model1(images,labels)
-                adv=self.model2(images)+images
-                shuffle=self.model3(images,labels)
+                pgd=self.model1(images)*0.1+images
+                adv=self.model2(images)*0.1+images
+                shuffle=self.model3(images)*0.1+images
 
                 pre,map_k=self.meta_g(images,pgd,adv,shuffle) 
                 pre_labels=self.tar(pre) #计算扰动后的图像的分类情况
                 pre_labels=torch.argmax(pre_labels,dim=1)
-
-                loss1 = -F.mse_loss(pre_labels.float(),labels.float())
+                if self.target==-1:
+                  f_labels = labels[torch.randperm(labels.size(0))]
+                else:
+                  f_labels = torch.ones_like(labels)*self.target
+                loss1 = F.mse_loss(pre_labels.float(),labels.float())
                 loss2 = F.mse_loss(pre,images)
 
                 L1=0 #正则化（暂时还没编）
@@ -52,7 +57,7 @@ class meta:
                 (loss2+loss1).backward()
                 self.optimizer.step()
             
-            print('Epoch [%d/%d],  Loss1: %.4f, Loss2 %.4f'%(epoch+1, epochs, loss1.item(),loss2.item()))
+            print('Epoch [%d/%d],  Loss1: %.4f, Loss2 %.4f'%(epoch, epochs, loss1.item(),loss2.item()))
 
 
 
